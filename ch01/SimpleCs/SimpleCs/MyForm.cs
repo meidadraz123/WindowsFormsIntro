5a799e54cc14f584f3539f02bdc009d51491dd28
meidad raz <meidad.raz@gmail.com>
2018-09-27 22:31:48 +0300

    Step 1 of a simple windows forms application VB & C#

---
+++ ch01/SimpleCs/SimpleCs/MyForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCs
+{
+    class MyForm : Form
+    {
+        public MyForm()
+        {
+            this.Text = "My Form";
+        }
+
+    }
+}
