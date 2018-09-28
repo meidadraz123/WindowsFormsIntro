f8cac7265e509e7f2412dae913c60a313e7695ce
meidad raz <meidad.raz@gmail.com>
2018-09-28 11:03:47 +0300

    CS - Added three controls

--- ch01/SimpleCs/SimpleCs/MyForm.cs
+++ ch01/SimpleCs/SimpleCs/MyForm.cs
@@ -9,9 +9,34 @@ namespace SimpleCs
 {
     class MyForm : Form
     {
+        private TextBox MessageTextBox;
+        private Label MessageLabel;
+        private Button ShowMessageButton;
+
         public MyForm()
         {
             this.Text = "My Form";
+
+            MessageTextBox = new TextBox();
+            MessageTextBox.Left = 25;
+            MessageTextBox.Top = 25;
+            MessageTextBox.Width = 200;
+            this.Controls.Add(MessageTextBox);
+
+            ShowMessageButton = new Button();
+            ShowMessageButton.Left = 25;
+            ShowMessageButton.Top = 75;
+            ShowMessageButton.Width = 200;
+            ShowMessageButton.Text = "Show Message";
+            this.Controls.Add(ShowMessageButton);
+
+            MessageLabel = new Label();
+            MessageLabel.Left = 25;
+            MessageLabel.Top = 125;
+            MessageLabel.Width = 200;
+            MessageLabel.Text = "[Label]";
+            this.Controls.Add(MessageLabel);
+                                                         
         }
 
     }
