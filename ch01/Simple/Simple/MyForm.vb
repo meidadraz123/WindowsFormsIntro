16cc386e5a1a3f0fa883e40e247760109dddba9b
meidad raz <meidad.raz@gmail.com>
2018-09-28 10:49:10 +0300

    VB - Adding basic controls to the form

--- ch01/Simple/Simple/MyForm.vb
+++ ch01/Simple/Simple/MyForm.vb
@@ -3,8 +3,32 @@
 Public Class MyForm
     Inherits Form
 
+    Private MessageTextBox As TextBox
+    Private MessageLabel As Label
+    Private ShowMessageButton As Button
+
     Public Sub New()
         Me.Text = "MyForm"
+
+        MessageTextBox = New TextBox()
+        MessageTextBox.Left = 25
+        MessageTextBox.Top = 25
+        MessageTextBox.Width = 200
+        Me.Controls.Add(MessageTextBox)
+
+        ShowMessageButton = New Button()
+        ShowMessageButton.Left = 25
+        ShowMessageButton.Top = 75
+        ShowMessageButton.Width = 200
+        ShowMessageButton.Text = "Show Message"
+        Me.Controls.Add(ShowMessageButton)
+
+        MessageLabel = New Label()
+        MessageLabel.Left = 25
+        MessageLabel.Top = 125
+        MessageLabel.Width = 200
+        MessageLabel.Text = "[Label]"
+        Me.Controls.Add(MessageLabel)
     End Sub
 
 
