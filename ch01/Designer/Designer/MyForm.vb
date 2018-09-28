fc6df15e0288fd7230c888b6cd4200674a08c65f
meidad raz <meidad.raz@gmail.com>
2018-09-28 12:00:36 +0300

    VB - basic app using the designer

---
+++ ch01/Designer/Designer/MyForm.vb
@@ -0,0 +1,5 @@
+﻿Public Class MyForm
+    Private Sub ShowMessageButton_Click(sender As Object, e As EventArgs) Handles ShowMessageButton.Click
+        MessageLabel.Text = MessageTextBox.Text
+    End Sub
+End Class
