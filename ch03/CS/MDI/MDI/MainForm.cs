dc03da9218416df4ecaed3b4c53a23fcad75255e
meidad raz <meidad.raz@gmail.com>
2018-09-29 13:25:07 +0300

    chapter 3 - MDI document

---
+++ ch03/CS/MDI/MDI/MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private int counter;
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var childForm = new ChildForm();
+            childForm.MdiParent = this;
+            counter++;
+            childForm.Text = "New Document " + counter;
+            childForm.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileHorizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Undo();
+            }
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Redo();
+            }
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Cut();
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Copy();
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Paste();
+            }
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.SelectAll();
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                var childForm = (ChildForm)this.ActiveMdiChild;
+                var dialog = new SaveFileDialog();
+                dialog.Filter = "Rich text file|*.rtf";
+                dialog.AddExtension = true;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    childForm.DocumentTextBox.SaveFile(dialog.FileName);
+                    childForm.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Rich text file|*.rtf";
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var childForm = new ChildForm();
+                childForm.DocumentTextBox.LoadFile(dialog.FileName);
+                childForm.Text = dialog.FileName;
+                childForm.MdiParent = this;
+                childForm.Show();
+            }
+        }
+    }
+}
---
+++ ch03/VB/MDI/MDI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MDI")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MDI")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6d6f8726-1d98-43e2-bdea-a3b49ace804c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
