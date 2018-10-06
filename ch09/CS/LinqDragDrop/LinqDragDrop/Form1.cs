b7ee50459e02c09389962378cd20c3358d5f2466
meidad raz <meidad.raz@gmail.com>
2018-10-06 11:44:15 +0300

    Chapter 9 - Data Sources and Dra-Drop Data Binding. save before removing data grid view and changing to details view

---
+++ ch09/CS/LinqDragDrop/LinqDragDrop/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqDragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NorthwindDataContext context = new NorthwindDataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            productBindingSource.DataSource = context.Products;
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            context.SubmitChanges();
+        }
+    }
+}
---
+++ ch09/VB/LinqDragDrop/LinqDragDrop/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private _context As New NorthwimdDataContext()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ProductBindingSource.DataSource = _context.Products
+
+    End Sub
+
+    Private Sub ProductBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ProductBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ProductBindingSource.EndEdit()
+        _context.SubmitChanges()
+    End Sub
+End Class
