8991627c44068c414bc7bda7b7aae666a3696831
meidad raz <meidad.raz@gmail.com>
2018-10-06 14:25:45 +0300

    Chapter 10 - ClickOnce installation - using file share

---
+++ ch10/VB/Explorer/Explorer/ExplorerForm.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExplorerForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ViewToolStripComboBox = New System.Windows.Forms.ToolStripComboBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.LargeImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.SmallImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.ViewToolStripComboBox})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(974, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(32, 22)
+        Me.ToolStripLabel1.Text = "View"
+        '
+        'ViewToolStripComboBox
+        '
+        Me.ViewToolStripComboBox.Items.AddRange(New Object() {"Large Icons", "Small Icons", "Details"})
+        Me.ViewToolStripComboBox.Name = "ViewToolStripComboBox"
+        Me.ViewToolStripComboBox.Size = New System.Drawing.Size(121, 25)
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 25)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TreeView1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ListView1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(974, 619)
+        Me.SplitContainer1.SplitterDistance = 324
+        Me.SplitContainer1.TabIndex = 1
+        '
+        'TreeView1
+        '
+        Me.TreeView1.BackColor = System.Drawing.Color.Khaki
+        Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(324, 619)
+        Me.TreeView1.TabIndex = 0
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.GreenYellow
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.LargeImageList = Me.LargeImageList
+        Me.ListView1.Location = New System.Drawing.Point(0, 0)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(646, 619)
+        Me.ListView1.SmallImageList = Me.SmallImageList
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'LargeImageList
+        '
+        Me.LargeImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.LargeImageList.ImageSize = New System.Drawing.Size(32, 32)
+        Me.LargeImageList.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'SmallImageList
+        '
+        Me.SmallImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.SmallImageList.ImageSize = New System.Drawing.Size(16, 16)
+        Me.SmallImageList.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'ExplorerForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(974, 644)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "ExplorerForm"
+        Me.Text = "Explorer-Style Application"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents LargeImageList As ImageList
+    Friend WithEvents SmallImageList As ImageList
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ViewToolStripComboBox As ToolStripComboBox
+End Class
